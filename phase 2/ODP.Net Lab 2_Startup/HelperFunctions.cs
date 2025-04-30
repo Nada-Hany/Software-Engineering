f29e41892cf20bc00893ef7167011bc4471b9449
Nouran Ahmed <ahmednoran200475@gmail.com>
2025-05-01 01:43:59 +0300

    Merge admin form V1 to Login form

---
+++ phase 2/ODP.Net Lab 2_Startup/HelperFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class HelperFunctions
+{
+	public HelperFunctions()
+	{
+
+	}
+
+	public static UserExists(string username, string password)
+	{
+
+	}
+}
---
+++ phase 2/ODP.Net Lab 2_Startup/ODP1_Connected_Start/MoviesForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ODP1_Connected_Start
+{
+    partial class MoviesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.movie_names = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Location = new System.Drawing.Point(85, 63);
+            this.categoryComboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(136, 24);
+            this.categoryComboBox.TabIndex = 0;
+            this.categoryComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // movie_names
+            // 
+            this.movie_names.AutoSize = true;
+            this.movie_names.Location = new System.Drawing.Point(85, 41);
+            this.movie_names.Name = "movie_names";
+            this.movie_names.Size = new System.Drawing.Size(51, 16);
+            this.movie_names.TabIndex = 1;
+            this.movie_names.Text = "Movies";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(85, 120);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(415, 219);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // MoviesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 390);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.movie_names);
+            this.Controls.Add(this.categoryComboBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MoviesForm";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MoviesForm_FormClosing);
+            this.Load += new System.EventHandler(this.MoviesForm_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.Label movie_names;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
